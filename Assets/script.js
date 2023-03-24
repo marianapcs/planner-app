b7b6ee2f7bdc5e6011c8a770ed691770f98cd4f2
Mariana  Serrão <marianaserrao@Marianas-MacBook-Air.local>
2023-03-24 14:47:10 +0100

    container was accessing an id instead of class, broke down the code so it was clearer to figure out what was wrong, table and rows append on screen, hours aren't passed in and style is bad

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function (){
   
     //defininf the main container and current day elements as JQuery objects
-    var mainContainer = $('#container');
+    var mainContainer = $('.container');
     var currentDay = $('#currentDay');
 
     //create an object representing today's date and time
@@ -15,61 +15,80 @@ $(document).ready(function (){
 
     currentDay.text(today.format('dddd, MMMM Do'));
 
-    var table = $('<div>').addClass('time-block');
-
-
-
-
-
-        //Want to loop over the hours of the day from 9-17h
-        for(let i=9; i < 18; i++) {
-
-             var row = $('<div>').addClass('row');
-            //convert the hour to a moment object and store it in a var 
-            var timeMoment = moment(i, ['HH']); 
+    
+    //Want to loop over the hours of the day from 9-17h
+    for(let i=9; i < 18; i++) {
         
+        //convert the hour to a moment object and store it in a var 
+        var timeMoment = moment(i, ['HH']); 
+        
+        
+        console.log(timeMoment.format('h A'));
+        
+        var table = $('<div>');
 
-            console.log(timeMoment.format('h A'));
+        var hour = $('<div>');
+        hour.addClass('hours');
+        var timeText = $("<p>");
+       
+
+
+        timeText.text(timeMoment.format('h A'));
+        timeText.append(timeText);
+
+        var userInfo = $("<input>");
+        userInfo.val(localStorage.getItem(`ButtonClick${i-9}`))
+
+
+        if (timeMoment < currentHour) {
+            userInfo.addClass("past userinfo");
+        } else if (timeMoment === currentHour) {
+            userInfo.addClass("present userinfo");
+        } else{
+            userInfo.addClass("future userinfo");
+        }
+        
+        console.log(userInfo);
+
+        console.log(submitButton);
+
+        //need to create a button for users to save info
+        var submitButton =$("<button>");
+        submitButton.addClass("saveBtn");
+        submitButton.text("Add");
+        submitButton.attr("buttonnum", i-9);
+        
+         // append timeText, userInfo and submitButton to the div
+         table.append(hour);
+         table.append(userInfo);
+         table.append(submitButton);
+
+         
+         table.addClass('row');
+        //  row.append(div);
+
+         userInfo.val(localStorage.getItem('ButtonClick'+(i-1)));
+
+         //appending Div to the main container
+         mainContainer.append(table);
+     }
+
+
+
+
+
+
+            //  var row = $('<div>').addClass('row');
 
             //create a div to hold the time slots, an text box and a submit button
-            var div =  $('<div>').addClass("hour");
+        
             //para element for time label 
-            var timeText = $("<p>").text(timeMoment.format('h A'));
-            div.append(timeText);
             //input element for users info for each time slot
-            var userInfo = $("<input>").val(localStorage.getItem(`ButtonClick${i-9}`));
-            //need to create a button for users to save info
-            var submitButton =$("<button>").addClass("saveBtn").text("Add").attr("buttonnum", i-9);
-
-
-            console.log(userInfo);
 
             // * Present timeblocks for standard business hours when the user scrolls down.
             
             //class for the input element based on time
 
-            if (timeMoment < today) {
-                userInfo.addClass("past userinfo");
-            } else if (timeMoment === today) {
-                userInfo.addClass("present userinfo");
-            } else{
-                userInfo.addClass("future userinfo");
-            }
-            
-            console.log(userInfo);
-
-            console.log(submitButton);
-           
-            // append timeText, userInfo and submitButton to the div
-            div.append(timeText, userInfo, submitButton);
-            row.append(div);
-            table.append(row);
-
-            userInfo.val(localStorage.getItem('ButtonClick'+(i-1)));
-
-            //appending Div to the main container
-        }
-            mainContainer.append(table);
 
         
 
